27cd82524eb9b86c5afb591370aca403eeb5463b
ABHINAND ULLAS P <145597216+Abhinand-Ullas@users.noreply.github.com>
2025-01-22 12:53:49 +0530

    tutorial screen added

--- lib/pages/tutorial_screen.dart
+++ lib/pages/tutorial_screen.dart
@@ -1,10 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:truelog/pages/homescreen.dart';
 
 class TutorialScreen extends StatelessWidget {
-  const TutorialScreen({super.key});
+  TutorialScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Tutorial",
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.w400, color: Colors.white),
+          ),
+        ),
+      ),
+      body: PageView(
+        children: [
+          _buildpage('Welcome to Notes App', 'Here you can take notes.'),
+          _buildpage('Double Tap to View',
+              'Double tap the title of a note to view its contents.'),
+          _buildpage('Long Press to Delete',
+              'Long press the title to show the delete button.'),
+          _buildpage('ontapdown to remove delete option',
+              'tap anywhere else to remove the delete button')
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      },
+      child: Text("Continue"),
+      ),
+    );
+  }
+
+  Widget _buildpage(String title, String content) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 20),
+        Text(
+          content,
+          style: TextStyle(fontSize: 16),
+        )
+      ],
+    );
   }
 }
