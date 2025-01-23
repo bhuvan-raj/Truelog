4c6f0552daa5b22e8b8514bb544755f89092574a
ABHINAND ULLAS P <145597216+Abhinand-Ullas@users.noreply.github.com>
2025-01-23 23:35:55 +0530

    further modifications in text alignment

--- lib/pages/empty_homepage.dart
+++ lib/pages/empty_homepage.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:truelog/pages/note_display.dart';
 
 class EmptyHomepage extends StatelessWidget {
   final screensize;
-  const EmptyHomepage({super.key,required this.screensize});
+  const EmptyHomepage({super.key, required this.screensize});
 
   @override
   Widget build(BuildContext context) {
+    final screenH = MediaQuery.of(context).size.height;
     return Center(
       child: Container(
         color: Color.fromARGB(69, 103, 101, 101),
@@ -14,11 +16,14 @@ class EmptyHomepage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('assets/main_image.jpg'),
-            SizedBox(height: 20),
-            Text(
-              'Create your first note!',
-              style: TextStyle(fontSize: 16, color: Colors.grey[300]),
+            Image.asset('assets/main_image.jpg', height: screenH * 0.75),
+            SizedBox(height: screenH * 0.05),
+            Expanded(
+              child: Text(
+                'Create your first note!',
+                style: TextStyle(
+                    fontSize: screenH * 0.022, color: Colors.grey[300]),
+              ),
             ),
           ],
         ),
--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -9,7 +9,7 @@ class FeedBack extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false, // Remove the default back button
+        automaticallyImplyLeading: false,
         backgroundColor: const Color.fromARGB(117, 103, 101, 101),
         elevation: 0,
         title: Text(
@@ -21,32 +21,34 @@ class FeedBack extends StatelessWidget {
           ),
         ),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_rounded), // Custom back button icon
+          icon: Icon(Icons.arrow_back_ios_rounded),
           onPressed: () {
-            Navigator.pop(context); // Go back to the previous screen
+            Navigator.pop(context);
           },
         ),
       ),
       body: Stack(
-        // for displaying text over image
         children: [
           Positioned(
             left: 0,
             right: 0,
-            top: screenH * 0.325,
+            top: screenH * 0.3,
             child: Container(
               padding: EdgeInsets.all(screenH * 0.02),
               decoration: BoxDecoration(
                 color: Colors.grey[800],
                 borderRadius: BorderRadius.circular(30),
               ),
-              child: Text(
-                "Need help with TrueLog? We're here for you! If you're experiencing any issues or have questions, feel free to reach out to us.Our Social Media handles are given below",
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: screenH * 0.02,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 1),
+                child: Text(
+                  "Need help with TrueLog? We're here for you! If you're experiencing any issues or have questions, feel free to reach out to us through our Social Media handles.",
+                  textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: screenH * 0.02,
+                  ),
                 ),
               ),
             ),
@@ -57,27 +59,22 @@ class FeedBack extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(
-                    height: screenH * 0.133), // to lower the container a bit
+                SizedBox(height: screenH * 0.133),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Image.asset(
                       'assets/crowned_boy.png',
-                      height: screenH * 0.30,
-                      width: screenW * 0.20,
+                      height: screenH * 0.25,
+                      width: screenW * 0.25,
                     ),
-                    SizedBox(
-                      // spacing between two images
-                      height: 30,
-                      width: screenW * 0.5,
-                    ),
+                    SizedBox(height: 30, width: screenW * 0.4),
                     Padding(
                       padding: const EdgeInsets.only(top: 15.0),
                       child: Image.asset(
                         'assets/boy.png',
-                        height: screenH * 0.30,
-                        width: screenW * 0.20,
+                        height: screenH * 0.25,
+                        width: screenW * 0.25,
                       ),
                     ),
                   ],
@@ -85,7 +82,7 @@ class FeedBack extends StatelessWidget {
               ],
             ),
           ),
-          Bottompart() //bottom part of the page is a separate page
+          Bottompart(),
         ],
       ),
     );
