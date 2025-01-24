df079dd4543f8ec42da5e04787687136f2095f63
ABHINAND ULLAS P <145597216+Abhinand-Ullas@users.noreply.github.com>
2025-01-24 13:35:30 +0530

    modified feedback page alignment

--- lib/pages/feedback.dart
+++ lib/pages/feedback.dart
@@ -9,7 +9,7 @@ class FeedBack extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
+        automaticallyImplyLeading: false, // Remove the default back button
         backgroundColor: const Color.fromARGB(117, 103, 101, 101),
         elevation: 0,
         title: Text(
@@ -21,34 +21,32 @@ class FeedBack extends StatelessWidget {
           ),
         ),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_rounded),
+          icon: Icon(Icons.arrow_back_ios_rounded), // Custom back button icon
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.pop(context); // Go back to the previous screen
           },
         ),
       ),
       body: Stack(
+        // for displaying text over image
         children: [
           Positioned(
             left: 0,
             right: 0,
-            top: screenH * 0.3,
+            top: screenH * 0.335,
             child: Container(
               padding: EdgeInsets.all(screenH * 0.02),
               decoration: BoxDecoration(
                 color: Colors.grey[800],
                 borderRadius: BorderRadius.circular(30),
               ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 1),
-                child: Text(
-                  "Need help with TrueLog? We're here for you! If you're experiencing any issues or have questions, feel free to reach out to us through our Social Media handles.",
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    fontSize: screenH * 0.02,
-                  ),
+              child: Text(
+                "Need help with TrueLog? We're here for you! If you're experiencing any issues or have questions, feel free to reach out to us through our Social Media handles.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 20,
                 ),
               ),
             ),
@@ -59,22 +57,27 @@ class FeedBack extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                SizedBox(height: screenH * 0.133),
+                SizedBox(
+                    height: screenH * 0.133), // to lower the container a bit
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Image.asset(
                       'assets/crowned_boy.png',
-                      height: screenH * 0.25,
-                      width: screenW * 0.25,
+                      height: screenH * 0.30,
+                      width: screenW * 0.20,
                     ),
-                    SizedBox(height: 30, width: screenW * 0.4),
+                    SizedBox(
+                      // spacing between two images
+                      height: 30,
+                      width: screenW * 0.5,
+                    ),
                     Padding(
                       padding: const EdgeInsets.only(top: 15.0),
                       child: Image.asset(
                         'assets/boy.png',
-                        height: screenH * 0.25,
-                        width: screenW * 0.25,
+                        height: screenH * 0.30,
+                        width: screenW * 0.20,
                       ),
                     ),
                   ],
@@ -82,7 +85,7 @@ class FeedBack extends StatelessWidget {
               ],
             ),
           ),
-          Bottompart(),
+          Bottompart() //bottom part of the page is a separate page
         ],
       ),
     );
